618c8c575dc2a774ce708543805a551857945fb3
adolfo <adlf.aldana@gmail.com>
2020-10-15 15:40:17 -0400

    componente de tarjetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { NoimagePipe } from './pipes/noimage.pipe';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { NoimagePipe } from './pipes/noimage.pipe';
     ArtistaComponent,
     NavbarComponent,
     NoimagePipe,
+    TarjetasComponent,
   ],
   imports: [HttpClientModule, BrowserModule, AppRoutingModule],
   providers: [],
